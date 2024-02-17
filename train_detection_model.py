fe434de694fc6d52e81b37f6204308e8bc9fa8cf
mehditayeb05 <134025163+mehditayeb05@users.noreply.github.com>
2024-02-17 18:09:03 +0100

    train_detection_model.py

--- train_detection_model.py
+++ train_detection_model.py
@@ -3,22 +3,22 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 import joblib
 
-# Charger les données d'entraînement avec des adresses IP
+# Load training data with IP addresses
 train_data = pd.read_csv('train_icmp_flood_ip_dataset.csv')
 
-# Prétraiter les données (vous devez adapter le prétraitement en fonction de vos besoins)
-# Ici, on convertit les adresses IP en représentation numérique
+# Preprocess the data (you need to adjust preprocessing as needed)
+# Here, we convert IP addresses into numerical representation
 train_data['source_ip'] = train_data['source_ip'].apply(lambda ip: int(ip.split('.')[-1]))
 
-# Utiliser le StandardScaler pour normaliser les données (vous pouvez adapter cela)
+# Use StandardScaler to normalize the data (you can adjust this as needed)
 scaler = StandardScaler()
 train_data[['source_ip']] = scaler.fit_transform(train_data[['source_ip']])
 
-# Créer et entraîner le modèle de régression logistique
+# Create and train the logistic regression model
 logistic_model = LogisticRegression(random_state=42)
 logistic_model.fit(train_data[['source_ip']], train_data['label'])
 
-# Sauvegarder le modèle
+# Save the model
 joblib.dump(logistic_model, 'trained_logistic_model_ip.pkl')
-# Sauvegarder le scaler utilisé lors de l'entraînement
+# Save the scaler used during training
 joblib.dump(scaler, 'trained_scaler_ip.pkl')
