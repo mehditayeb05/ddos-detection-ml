3ca0144197321a993562de501155f92feeaf2603
mehditayeb05 <134025163+mehditayeb05@users.noreply.github.com>
2024-02-17 18:07:55 +0100

    run_detection.py

--- run_detection.py
+++ run_detection.py
@@ -3,40 +3,40 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score
 import joblib
 
-# Charger le modèle entraîné
+# Load the trained model
 model = joblib.load('trained_logistic_model_ip.pkl')
 
-# Charger les données de test avec des adresses IP (utilisation du même fichier que les données d'entraînement)
+# Load the test data with IP addresses (using the same file as the training data)
 test_data = pd.read_csv('test_icmp_flood_ip_dataset.csv')
 
-# Charger le scaler utilisé lors de l'entraînement
+# Load the scaler used during training
 scaler = joblib.load('trained_scaler_ip.pkl')
 
 def preprocess_data(data, scaler):
-    # Effectuer le prétraitement nécessaire sur les données
+    # Perform necessary preprocessing on the data
     data['source_ip'] = data['source_ip'].apply(lambda ip: int(ip.split('.')[-1]))
     data[['source_ip']] = scaler.transform(data[['source_ip']])
     return data
 
 def detect_attack(data):
-    # Appliquer le modèle pour détecter les attaques
+    # Apply the model to detect attacks
     predictions = model.predict(data[['source_ip']])
     return predictions
 
 if __name__ == "__main__":
-    # Prétraiter les données
+    # Preprocess the data
     test_data = preprocess_data(test_data, scaler)
 
-    # Détecter les attaques
+    # Detect attacks
     predictions = detect_attack(test_data)
 
-    # Afficher les résultats ou prendre d'autres mesures nécessaires
+    # Display the results or take other necessary actions
     print(predictions)
     results = pd.DataFrame({'Actual': test_data['label'], 'Predicted': predictions})
 
-    # Imprimer une partie du DataFrame pour une analyse plus détaillée
+    # Print part of the DataFrame for further analysis
     print(results.head(50))  
 
-    # Calculer et afficher l'accuracy
+    # Calculate and display the accuracy
     accuracy = accuracy_score(test_data['label'], predictions)
     print(f'Accuracy: {accuracy}')
